9863b4e09752a0a70d568d95a1fb23eb74c746a2
felipe <felipe.higino.ps@gmail.com>
2021-02-22 21:33:53 -0300

    creating file reader
    - concepts:
    - pipe operator
    - pattern matching
    - private access functions
    - File, String, Enum modules

---
+++ phoenixApp/lib/phoenixApp/mycsv.ex
@@ -0,0 +1,19 @@
+defmodule PhoenixApp.Mycsv do
+  def sum_from_csv(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, file}) do
+    sum =
+      file
+      |> String.split(",")
+      |> Enum.map(fn x -> String.to_integer(x) end)
+      |> Enum.sum()
+
+    {:ok, %{result: sum}}
+  end
+
+  defp handle_file({:error, _any}), do: {:error, "Invalid file"}
+end
